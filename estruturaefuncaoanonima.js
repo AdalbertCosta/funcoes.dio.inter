8d58a45d036162c17ef6c7bdc72e7c16d312cb38
AdalbertCosta <contato@adalbertocosta.com>
2022-01-02 07:49:16 -0300

    conclusão

--- estruturaefuncaoanonima.js
+++ estruturaefuncaoanonima.js
@@ -19,22 +19,3 @@ const soma = function (a,b) {
 }
 
 soma(2,10)
-
-// Função Autoinvocável
-//Uma função anônima entre parênteses, seguida por outro par de parênteses, que representa sua chamada.
-// Também pode ser utilizada com parâmetros ou armazenada em uma variável.
-(
-  function (a,b){
-    return a + b;
-  }
-  )(1,2); //3
-//..............................
-const soma3 = (
-  function () {
-    return a +b;
-  }
-)(1,2);
-console.log(soma3) //3
-
-// Função Autoinvocável
-
